ae2a65f163f8f6ec1b3dad7bbf84ee559a66ad8b
Jordy <Jordy.tak@gmail.com>
2018-09-30 16:53:24 +0200

    now its good, I hope

---
+++ identitygithub/identitygithub/Controllers/HomeController.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PluralsightDemo.Models;
+
+namespace PluralsightDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<PluralsightUser> userManager;
+        private readonly IUserClaimsPrincipalFactory<PluralsightUser> claimsPrincipalFactory;
+
+        public HomeController(UserManager<PluralsightUser> userManager,
+            IUserClaimsPrincipalFactory<PluralsightUser> claimsPrincipalFactory)
+        {
+            this.userManager = userManager;
+            this.claimsPrincipalFactory = claimsPrincipalFactory;
+        }
+        
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //[HttpGet]
+        //public IActionResult Register()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Register(RegisterModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = await userManager.FindByNameAsync(model.UserName);
+
+        //        if (user == null)
+        //        {
+        //            user = new PluralsightUser
+        //            {
+        //                Id = Guid.NewGuid().ToString(),
+        //                UserName = model.UserName,
+        //                Email = model.UserName
+        //            };
+
+        //            var result = await userManager.CreateAsync(user, model.Password);
+
+        //            if (result.Succeeded)
+        //            {
+        //                var token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+        //                var confirmationEmail = Url.Action("ConfirmEmailAddress", "Home",
+        //                    new {token = token, email = user.Email}, Request.Scheme);
+        //                System.IO.File.WriteAllText("confirmationLink.txt", confirmationEmail);
+        //            }
+        //            else
+        //            {
+        //                foreach (var error in result.Errors)
+        //                {
+        //                    ModelState.AddModelError("", error.Description);
+        //                }
+
+        //                return View();
+        //            }
+        //        }
+
+        //        return View("Success");
+        //    }
+
+        //    return View();
+        //}
+
+        ////[HttpGet]
+        ////public async Task<IActionResult> ConfirmEmailAddress(string token, string email)
+        ////{
+        ////    var user = await userManager.FindByEmailAsync(email);
+
+        ////    if (user != null)
+        ////    {
+        ////        var result = await userManager.ConfirmEmailAsync(user, token);
+
+        ////        if (result.Succeeded)
+        ////        {
+        ////            return View("Success");
+        ////        }
+        ////    }
+
+        ////    return View("Error");
+        ////}
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var user = await userManager.FindByNameAsync(model.UserName);
+
+                if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    //if (!await userManager.IsEmailConfirmedAsync(user))
+                    //{
+                    //    ModelState.AddModelError("","Email is not confirmed");
+                    //    return View();
+                    //}
+
+                    var principal = await claimsPrincipalFactory.CreateAsync(user);
+
+                    await HttpContext.SignInAsync("Identity.Application", principal);
+
+                    //inlog logic
+                    var isAdmin = await userManager.IsInRoleAsync(user,Constants.AdministratorRole);
+
+                    if (isAdmin) {
+
+                        return RedirectToAction("Admin");
+                        //sdfdsfsdfsdf
+                    }
+                
+                    return RedirectToAction("Leerling");
+
+
+                }
+
+                ModelState.AddModelError("", "Invalid UserName or Password");
+            }
+
+            return View();
+        }
+
+
+
+        //// misschien voor later
+        //[HttpGet]
+        //public IActionResult ForgotPassword()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> ForgotPassword(ForgotPasswordModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = await userManager.FindByEmailAsync(model.Email);
+
+        //        if (user != null)
+        //        {
+        //            var token = await userManager.GeneratePasswordResetTokenAsync(user);
+        //            var resetUrl = Url.Action("ResetPassword", "Home",
+        //                new {token = token, email = user.Email}, Request.Scheme);
+
+        //            System.IO.File.WriteAllText("resetLink.txt", resetUrl);
+        //        }
+        //        else
+        //        {
+        //            // email user and inform them that they do not have an account
+        //        }
+
+        //        return View("Success");
+        //    }
+        //    return View();
+        //}
+
+        //[HttpGet]
+        //public IActionResult ResetPassword(string token, string email)
+        //{
+        //    return View(new ResetPasswordModel { Token = token, Email = email });
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> ResetPassword(ResetPasswordModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = await userManager.FindByEmailAsync(model.Email);
+
+        //        if (user != null)
+        //        {
+        //            var result = await userManager.ResetPasswordAsync(user, model.Token, model.Password);
+
+        //            if (!result.Succeeded)
+        //            {
+        //                foreach (var error in result.Errors)
+        //                {
+        //                    ModelState.AddModelError("", error.Description);
+        //                }
+        //                return View();
+        //            }
+        //            return View("Success");
+        //        }
+        //        ModelState.AddModelError("", "Invalid Request");
+        //    }
+        //    return View();
+        //}
+    }
+}
---
+++ identitygithub/identitygithub/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PluralsightDemo
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            
+               // @"Data Source=Server=(localdb)\\mssqllocaldb;database=PluralsightDemo.PluralsightUser;trusted_connection=yes;";
+            //var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            //services.AddDbContext<PluralsightUserDbContext>(opt => opt.UseSqlServer(connectionString,
+            //    sql => sql.MigrationsAssembly(migrationAssembly)));
+
+            services.AddDbContext<PluralsightUserDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+            services.AddIdentity<PluralsightUser, IdentityRole>(options =>
+                {
+                    //options.SignIn.RequireConfirmedEmail = true;
+                    options.Tokens.EmailConfirmationTokenProvider = "emailconf";
+
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredUniqueChars = 4;
+
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<PluralsightUserDbContext>()
+                .AddDefaultTokenProviders()
+                .AddTokenProvider<EmailConfirmationTokenProvider<PluralsightUser>>("emailconf")
+                .AddPasswordValidator<DoesNotContainPasswordValidator<PluralsightUser>>();
+
+            services.AddScoped<IUserClaimsPrincipalFactory<PluralsightUser>,
+                PluralsightUserClaimsPrincipalFactory>();
+
+            services.Configure<DataProtectionTokenProviderOptions>(options =>
+                options.TokenLifespan = TimeSpan.FromHours(3));
+            services.Configure<EmailConfirmationTokenProviderOptions>(options =>
+                options.TokenLifespan = TimeSpan.FromDays(2));
+
+            services.ConfigureApplicationCookie(options => options.LoginPath = "/Home/Login");
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Login}/{id?}");
+            });
+        }
+    }
+}

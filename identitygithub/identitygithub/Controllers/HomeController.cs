002ec3ac0558ed7b7b3ad91ebd8a21739d34fdb9
Jordy <Jordy.tak@gmail.com>
2018-12-13 02:11:26 +0100

    changes

--- identitygithub/identitygithub/Controllers/HomeController.cs
+++ identitygithub/identitygithub/Controllers/HomeController.cs
@@ -12,6 +12,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;// dit is nieuw
+//using MailKit.Net.Smtp;
+//using MailKit;
+//using MimeKit;
 using PluralsightDemo.Models;
 
 namespace PluralsightDemo.Controllers
@@ -503,86 +506,116 @@ namespace PluralsightDemo.Controllers
 
 
 
-
-
-
-
-
         //[HttpGet]
         //[Authorize(Roles = Constants.AdministratorRole)]
-        //public IActionResult ConvertToStudent(string Username)
+        //public IActionResult SendInvitation()
         //{
+        //    var message = new MimeMessage();
+        //    message.From.Add(new MailboxAddress("jordy","jordy.tak@gmail.com"));
 
-        //    if (Username != null) {
 
-        //        return RedirectToAction("Adminn", "Home");
-        //    }
+        //    message.To.Add(new MailboxAddress("jordy", "jordy.tak@gmail.com"));
 
-        //    return View("Success");
-        //}
+        //    message.Subject = "Email using asp.net core";
 
-        //// misschien voor later
-        //[HttpGet]
-        //public IActionResult ForgotPassword()
-        //{
-        //    return View();
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> ForgotPassword(ForgotPasswordModel model)
-        //{
-        //    if (ModelState.IsValid)
+        //    message.Body = new TextPart("plain")
         //    {
-        //        var user = await userManager.FindByEmailAsync(model.Email);
+        //        Text = "Im using Mailkit nuget package to send email easily"
+        //    };
 
-        //        if (user != null)
-        //        {
-        //            var token = await userManager.GeneratePasswordResetTokenAsync(user);
-        //            var resetUrl = Url.Action("ResetPassword", "Home",
-        //                new {token = token, email = user.Email}, Request.Scheme);
 
-        //            System.IO.File.WriteAllText("resetLink.txt", resetUrl);
-        //        }
-        //        else
-        //        {
-        //            // email user and inform them that they do not have an account
-        //        }
+        //    using (var client = new SmtpClient()) {
 
-        //        return View("Success");
+        //        client.ServerCertificateValidationCallback = (s, c, h, e) => true;// nieuwe lijn
+        //        client.Connect("smtp.gmail.com", 587, false);
+        //        client.Authenticate("jordy.tak@gmail.com", "jordy5335748");
+        //        client.Send(message);
+        //        client.Disconnect(true);
+        //       // client.ServerCertificateValidationCallback=
         //    }
+
         //    return View();
+
         //}
 
-        //[HttpGet]
-        //public IActionResult ResetPassword(string token, string email)
-        //{
-        //    return View(new ResetPasswordModel { Token = token, Email = email });
-        //}
 
-        //[HttpPost]
-        //public async Task<IActionResult> ResetPassword(ResetPasswordModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var user = await userManager.FindByEmailAsync(model.Email);
 
-        //        if (user != null)
-        //        {
-        //            var result = await userManager.ResetPasswordAsync(user, model.Token, model.Password);
 
-        //            if (!result.Succeeded)
-        //            {
-        //                foreach (var error in result.Errors)
-        //                {
-        //                    ModelState.AddModelError("", error.Description);
-        //                }
-        //                return View();
-        //            }
-        //            return View("Success");
-        //        }
-        //        ModelState.AddModelError("", "Invalid Request");
-        //    }
-        //    return View();
-        //}
-    }
+            //[HttpGet]
+            //[Authorize(Roles = Constants.AdministratorRole)]
+            //public IActionResult ConvertToStudent(string Username)
+            //{
+
+            //    if (Username != null) {
+
+            //        return RedirectToAction("Adminn", "Home");
+            //    }
+
+            //    return View("Success");
+            //}
+
+            //// misschien voor later
+            //[HttpGet]
+            //public IActionResult ForgotPassword()
+            //{
+            //    return View();
+            //}
+
+            //[HttpPost]
+            //public async Task<IActionResult> ForgotPassword(ForgotPasswordModel model)
+            //{
+            //    if (ModelState.IsValid)
+            //    {
+            //        var user = await userManager.FindByEmailAsync(model.Email);
+
+            //        if (user != null)
+            //        {
+            //            var token = await userManager.GeneratePasswordResetTokenAsync(user);
+            //            var resetUrl = Url.Action("ResetPassword", "Home",
+            //                new {token = token, email = user.Email}, Request.Scheme);
+
+            //            System.IO.File.WriteAllText("resetLink.txt", resetUrl);
+            //        }
+            //        else
+            //        {
+            //            // email user and inform them that they do not have an account
+            //        }
+
+            //        return View("Success");
+            //    }
+            //    return View();
+            //}
+
+            //[HttpGet]
+            //public IActionResult ResetPassword(string token, string email)
+            //{
+            //    return View(new ResetPasswordModel { Token = token, Email = email });
+            //}
+
+            //[HttpPost]
+            //public async Task<IActionResult> ResetPassword(ResetPasswordModel model)
+            //{
+            //    if (ModelState.IsValid)
+            //    {
+            //        var user = await userManager.FindByEmailAsync(model.Email);
+
+            //        if (user != null)
+            //        {
+            //            var result = await userManager.ResetPasswordAsync(user, model.Token, model.Password);
+
+            //            if (!result.Succeeded)
+            //            {
+            //                foreach (var error in result.Errors)
+            //                {
+            //                    ModelState.AddModelError("", error.Description);
+            //                }
+            //                return View();
+            //            }
+            //            return View("Success");
+            //        }
+            //        ModelState.AddModelError("", "Invalid Request");
+            //    }
+            //    return View();
+            //}
+        }
 }
